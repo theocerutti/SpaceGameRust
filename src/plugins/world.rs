9d5161d5867acfbbadb763fad2aa164b2dca55b4
Théo Cerutti <theo.cerutti@epitech.eu>
2023-06-09 12:46:16 +0200

    Fix entity destruction off screen

--- src/plugins/world.rs
+++ src/plugins/world.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use crate::components::components::DestroyLeaveScreen;
+use crate::plugins::player::Player;
+use crate::plugins::projectile::Projectile;
 use crate::state::GameState;
 
 pub struct WorldPlugin;
@@ -14,13 +17,31 @@ impl Plugin for WorldPlugin {
     }
 }
 
-fn destroy_leave_screen(mut commands: Commands, mut query: Query<(Entity, &Transform), With<Projectile>>, window: Query<&Window, With<PrimaryWindow>>) {
+fn destroy_leave_screen(
+    mut commands: Commands,
+    mut query: Query<(Entity, &Transform), With<DestroyLeaveScreen>>,
+    player_query: Query<&Transform, (With<Player>, Without<DestroyLeaveScreen>)>,
+    window: Query<&Window, With<PrimaryWindow>>
+) {
     let window = window.get_single().unwrap();
+    let player_transform = player_query.get_single();
 
-    for (entity, transform) in query.iter_mut() {
-        // destroy entity if it goes off screen
-        if transform.translation.x < -window.width() / 2. || transform.translation.x > window.width() / 2. || transform.translation.y < -window.height() / 2. || transform.translation.y > window.height() / 2. {
-            commands.entity(entity).despawn();
+    if let Ok(player_transform) = player_transform {
+        let destroy_size = Vec2 {
+            x: window.width() * 2,
+            y: window.height() * 2
+        };
+
+        for (entity, transform) in query.iter_mut() {
+            // destroy entity if it goes off screen
+            let entity_normalized_pos = Vec2 {
+                x: f32::abs(transform.translation.x - player_transform.translation.x),
+                y: f32::abs(transform.translation.y - player_transform.translation.y),
+            };
+            if entity_normalized_pos.x < 0. || entity_normalized_pos.x > destroy_size.x || entity_normalized_pos.y < 0. || entity_normalized_pos.y > destroy_size.y {
+                commands.entity(entity).despawn();
+            }
         }
     }
 }
+
